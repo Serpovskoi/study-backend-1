fe2d27202316a4b7f2fa4fbfbc515c10f1941f83
Alexey Shulga <shulga@perx.ru>
2025-02-16 16:40:03 +0300

    ЛР1. Создание веб приложения

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Включаем поддержку статических файлов
+app.UseStaticFiles();
 
+// Главная страница
+//app.MapGet("/", () => Results.File("wwwroot/pages/index.html", "text/html"));
+app.MapGet("/", async context =>{ await context.Response.SendFileAsync("wwwroot/pages/index.html"); });
+
+// Дополнительные страницы
+app.MapGet("/about", async context =>{ await context.Response.SendFileAsync("wwwroot/pages/about.html"); });
+app.MapGet("/wikipedia", async context =>{ await context.Response.SendFileAsync("wwwroot/pages/wikipedia.html"); });
+app.MapGet("/habr", async context =>{ await context.Response.SendFileAsync("wwwroot/pages/habr.html"); });
+
+// Запускаем сервер
 app.Run();
